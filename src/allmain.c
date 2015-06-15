3a96904ebbc85385e43a02a27bbc72ea1c94a3f8
Mauris Van Hauwe <0nooodl@gmail.com>
2015-06-15 21:58:38 +0200

    "Welcome to fNetHack!"

--- src/allmain.c
+++ src/allmain.c
@@ -675,8 +675,8 @@ boolean new_game;	/* false => restoring an old game */
 	     currentgend != flags.initgend))
 	Sprintf(eos(buf), " %s", genders[currentgend].adj);
 
-    pline(new_game ? "%s %s, welcome to NetHack!  You are a%s %s %s."
-		   : "%s %s, the%s %s %s, welcome back to NetHack!",
+    pline(new_game ? "%s %s, welcome to fNetHack!  You are a%s %s %s."
+		   : "%s %s, the%s %s %s, welcome back to fNetHack!",
 	  Hello((struct monst *) 0), plname, buf, urace.adj,
 	  (currentgend && urole.name.f) ? urole.name.f : urole.name.m);
 }
